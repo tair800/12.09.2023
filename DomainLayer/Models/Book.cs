8ae634bc0a441a7d0feb25175cea0f4e434f421e
tair800 <tahiraa@code.edu.az>
2023-12-09 13:23:11 +0400

    salam

---
+++ DomainLayer/Models/Book.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace DomainLayer.Models
+{
+    internal class Book : Product
+    {
+        public string AuthorName { get; set; }
+        public int PageCount { get; set; }
+
+        public override void Sell()
+        {
+            if (Count == 0)
+            {
+                Console.WriteLine("ProductCountIsZeroException");
+
+            }
+            
+        }
+        public Book(string name, int price, string authorNAme,int pageCount)
+        {
+            Name= name;
+            Price= price;
+            AuthorName= authorNAme;
+            PageCount= pageCount;
+            
+        }
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Book ID: {Id}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Author: {AuthorName}");
+            Console.WriteLine($"Page Count: {PageCount}");
+            Console.WriteLine($"Price: {Price}");
+            Console.WriteLine($"Count: {Count}");
+            Console.WriteLine($"Total Income: {TotalIncome}");
+        }
+    }
+}
---
+++ DomainLayer/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace DomainLayer.Models
+{
+    public abstract class Product
+    {
+     public int Id { get; }
+    public string Name { get; set; }
+    public double Price {  get; set; }
+    public int Count { get; set; }
+    public double TotalIncome { get; set; }
+        public abstract void Sell();
+        public abstract void ShowInfo();
+    }
+}
